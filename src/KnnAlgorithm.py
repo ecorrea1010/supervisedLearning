12764ea42dc94d39c1ab25a9287bfc64a60c1eb2
Eduardo Correa <correaherreraeduardo@gmail.com>
2023-05-11 23:34:48 -0500

    feat: knn algorithm

---
+++ src/KnnAlgorithm.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+global data
+def getData():
+    global data
+    data = pd.read_csv('files/covid.csv')
+
+def prepareData():
+    global data
+    #print(data.isna().sum())
+    features = [
+        'Dry-Cough',
+        'Difficulty-in-Breathing',
+        'Sore-Throat',
+        'Runny-Nose',
+        'Fever',
+        'Tiredness'
+    ]
+    target = ['Infected']
+    independentTraining, independentTest, dependentTraining, dependentTest = train_test_split(data[features], data[target], random_state=0)
+    return {
+        'independentTraining': independentTraining,
+        'independentTest': independentTest,
+        'dependentTraining': dependentTraining,
+        'dependentTest': dependentTest
+    }
+
+def model(dataModel):
+    knn = KNeighborsClassifier(n_neighbors=3)
+    dependentTraining = np.ravel(dataModel['dependentTraining'])
+    knn.fit(dataModel['independentTraining'], dependentTraining)
+    prediction = knn.predict(dataModel['independentTest'])
+    accuracy = knn.score(dataModel['independentTest'], dataModel['dependentTest'])
+    return {
+        'prediction': prediction,
+        'accuracy': accuracy
+    }
+
+def message(prediction):
+    numberOfPredictions = [int(predictions) for predictions in prediction['prediction']]
+    accuracyRound = round(prediction['accuracy'], 2)
+    effectiveness = accuracyRound * 100
+    infected = numberOfPredictions.count(1)
+    noInfected = numberOfPredictions.count(0)
+    message = 'This is the prediction of the model:'
+    message += '\n'
+    message += 'Infected: ' + str(infected)
+    message += '\n'
+    message += 'No infected: ' + str(noInfected)
+    message += '\n'
+    message += 'The effectiveness of the model was: ' + str(effectiveness) + '%'
+    print(message)
+
+def run():
+    getData()
+    prediction = model(prepareData())
+    message(prediction)
+
+if __name__ == '__main__':
+    run()
